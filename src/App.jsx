aaaa632994da0f5ee048e6a0d2e3c4c63522d957
christianosilvaf <christianosilvaf@gmail.com>
2023-08-08 18:20:08 -0500

    Second Advance

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,46 @@ import Weather from './components/Weather';
 function App() {
   const [weatherInfo,setweatherInfo]=useState(null);
   const [countryName,setcountryName]=useState(null);
+  
+  const IconRef=weatherInfo?.weather[0].icon;
+  const IconRefbg=IconRef?.slice(2,3)+IconRef?.slice(1,2);
+  
+  const bg={d1:"bg-[url(/images/01d.jpg)]",
+            d2:"bg-[url(/images/02d.jpg)]",
+            d3:"bg-[url(/images/03d.jpg)]",
+            d4:"bg-[url(/images/04d.jpg)]",
+            d5:"bg-[url(/images/05d.jpg)]",
+            d6:"bg-[url(/images/06d.jpg)]",
+            d7:"bg-[url(/images/07d.jpg)]",
+            d8:"bg-[url(/images/08d.jpg)]",
+            d9:"bg-[url(/images/09d.jpg)]",
+            n1:"bg-[url(/images/01n.jpg)]",
+            n2:"bg-[url(/images/02n.jpg)]",
+            n3:"bg-[url(/images/03n.jpg)]",
+            n4:"bg-[url(/images/04n.jpg)]",
+            n5:"bg-[url(/images/05n.jpg)]",
+            n6:"bg-[url(/images/06n.jpg)]",
+            n7:"bg-[url(/images/07n.jpg)]",
+            n8:"bg-[url(/images/08n.jpg)]",
+            n9:"bg-[url(/images/09n.jpg)]",
+}
 
+  
   const success=(pos)=>{
     const lat=pos.coords.latitude;
     const lon=pos.coords.longitude;
     const API_KEY="7f518877607f4511f7f13c0b40f428ba";
     const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    
 
+    
+    
     axios.get(url)
-      .then(({data})=>setweatherInfo(data))
-      .catch((err)=>console.log(err))
+      .then(({data})=> setweatherInfo(data))
+      .catch((err)=> console.log(err));
   };
 
   const url2=` http://api.worldbank.org/v2/country/${weatherInfo?.sys.country}?format=json`;
-
+    
     axios.get(url2)
       .then((data2)=> setcountryName(data2.data[1][0].name))
       .catch((err)=> console.log(err));
@@ -30,7 +55,7 @@ function App() {
   },[]);
 
   return (
-    <main className='bg-cyan-300 min-h-screen max-w-screen text-white font-lato flex justify-center items-center'>
+    <main className={`${bg[IconRefbg]} bg-center bg-cover min-h-screen max-w-screen text-black font-lato flex justify-center items-center`}>
       <Weather weatherInfo={weatherInfo} countryname={countryName}/>
     </main>
   )
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,47 +6,67 @@ const Weather = ({weatherInfo,countryname}) => {
     const TempCelsius= (weatherInfo?.main.temp-273.15).toFixed(1);
     const TempFarenheit= (TempCelsius*9/5+32).toFixed(1);
     const IconRef=weatherInfo?.weather[0].icon;
+    const FeelslikeCel=(weatherInfo?.main.feels_like-273.15).toFixed(1);
+    const FeelslikeFar=(FeelslikeCel*9/5+32).toFixed(1);
+    const velm_s=weatherInfo?.wind.speed;
+    const velkm_h=(velm_s*3600/1000).toFixed(1);
+    const prehpa=weatherInfo?.main.pressure;
+    const preAtm=(0.00099*prehpa).toFixed(3);
 
     const [Temp, setTemp] = useState(true);
+    const [vel, setVel] = useState(true);
+    const [pre, setPre] = useState(true);
     
     const handleTemp =()=>{
         Temp ? setTemp(false) : setTemp(true);
+    }
+
+    const handlevel =()=>{
+        vel ? setVel(false) : setVel(true);
+    }
+
+    const handlepre =()=>{
+        pre ? setPre(false) : setPre(true);
     }
 
     return (
     <section className='text-center min-h-screen w-[350px] flex flex-col place-content-around items-center'>
         <h2>{weatherInfo?.name}, {countryname}</h2>
         <section className='flex flex-col gap-y-10'>
-            <section className='bg-white/30 backdrop-blur-lg rounded-2xl place-content-evenly gap-10'>
+            <section className='bg-white/25 backdrop-blur-lg rounded-2xl place-content-evenly gap-10 p-5'>
                 <h3>State: {weatherInfo?.weather[0].description}</h3>
-                <div className='flex items-center place-content-evenly'>
-                <span> Temperature: <br/> {Temp? TempCelsius: TempFarenheit} {Temp ? "°C": "°F"}</span>
-                <div ><img src={`https://openweathermap.org/img/wn/${IconRef}@4x.png`} alt="" /></div>
+                <div className='flex items-center place-content-evenly m-2'>
+                <div className='flex flex-col items-center place-content-evenly'>   
+                    <span> Temperature: <br/> {Temp? TempCelsius: TempFarenheit} {Temp ? "°C": "°F"}</span>
+                    <span> Feels like: <br/> {Temp? FeelslikeCel: FeelslikeFar} {Temp ? "°C": "°F"}</span>
+                </div> 
+                <div className='bg-black/30 rounded-full'><img src={`https://openweathermap.org/img/wn/${IconRef}@4x.png`} alt="" /></div>
                 </div>
                 
             </section>
 
-            <section className='grid grid-cols-3 items-center place-content-around '>
-                <div className='flex flex-col items-center gap-y-3'>
+            <section className='grid grid-cols-3 items-center place-content-around gap-x-2'>
+
+                <div  onClick={handlevel} className='bg-white/25 backdrop-blur-lg rounded-2xl flex flex-col items-center gap-y-3 hover:shadow-[0_3px_0px_rgba(0,0,0,0.3)] rounded-2xl p-2'>
                     <img className='object-cover h-10 w-10' src={"/images/wind.svg"} alt="" />
-                    <span>{weatherInfo?.wind.speed} m/s</span>
+                    <span>{vel? velm_s: velkm_h}{vel? " m/s": " km/h"}</span>
                 </div>
 
-                <div className='flex flex-col items-center gap-y-3'>
+                <div className='bg-white/25 backdrop-blur-lg rounded-2xl flex flex-col items-center gap-y-3  rounded-2xl p-2'>
                     <img className='object-cover h-10 w-10' src={"/images/humidity.svg"} alt="" />
                     <span>{weatherInfo?.main.humidity} %</span>
                 </div>
 
-                <div className='flex flex-col items-center gap-y-3'>
+                <div onClick={handlepre} className='bg-white/25 backdrop-blur-lg rounded-2xl flex flex-col items-center gap-y-3 hover:shadow-[0_3px_0px_rgba(0,0,0,0.3)] rounded-2xl p-2'>
                     <img className='object-cover h-10 w-10' src={"/images/pressure.svg"} alt="" />
-                    <span>{weatherInfo?.main.pressure} hPa</span>
+                    <span>{pre? prehpa: preAtm} {pre? " hpa": " Atm"}</span>
                 </div>
                 
             </section>
             
         </section>
 
-        <button onClick={handleTemp} className='bg-white rounded-2xl w-[50%]  text-black'> Change Temperature Units to: {Temp? "°F": "°C"}</button>
+        <button onClick={handleTemp} className='bg-white/25 backdrop-blur-lg rounded-2xl w-[50%]  text-black hover:shadow-[0_3px_0px_rgba(0,0,0,0.3)]'> Change Temperature Units to: {Temp? "°F": "°C"}</button>
 
     </section>
 )
